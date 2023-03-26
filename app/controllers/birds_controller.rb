b9d5ca1a4a6732bb6eafd80521872fcaebb4a399
King <kelkab46@gmail.com>
2023-03-26 10:47:44 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,8 +9,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+ 
   end
 
   # GET /birds/:id
@@ -22,8 +23,9 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+
   end
 
   # DELETE /birds/:id
@@ -45,6 +47,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
